9c25bafd37daeb7aa27c668f6c972431e68f98e4
Vartan14 <89924019+Vartan14@users.noreply.github.com>
2022-10-10 02:02:51 +0300

    Add files via upload

--- lab1/multi_way_merge.py
+++ lab1/multi_way_merge.py
@@ -39,13 +39,12 @@ class MultiWayMerge:
         self.create_file()
 
         files_b_paths = []
+        files_c_paths = []
+
         for i in range(self.n_files):
             files_b_paths.append(f"Files/B{i + 1}.bin")
-
-        files_c_paths = []
-        for i in range(self.n_files):
             files_c_paths.append(f"Files/C{i + 1}.bin")
-        
+
         # create C files
         for path in files_c_paths:
             with open(path, "wb") as file:
@@ -67,18 +66,18 @@ class MultiWayMerge:
             shutil.copy(files_c_paths[0], self.path_d)
 
     @staticmethod
-    def is_sorted(init_file, file_b1, file_c1):
+    def is_sorted(init_file: str, file_b1: str, file_c1: str) -> bool:
         return os.path.getsize(init_file) == os.path.getsize(file_b1) or \
                os.path.getsize(init_file) == os.path.getsize(file_c1)
 
     @staticmethod
-    def is_all_empty(input_files: list) -> bool:
+    def are_all_empty(input_files: list) -> bool:
         for file in input_files:
             if file.curr:
                 return False
         return True
 
-    def first_distribution(self, files_b_paths):
+    def first_distribution(self, files_b_paths: list):
         # open files
         a = ReadFromFile(self.path_a)
         files_B = []
@@ -101,7 +100,7 @@ class MultiWayMerge:
         for file in files_B:
             file.close()
 
-    def merge(self, input_files_paths, output_files_paths):
+    def merge(self, input_files_paths: list, output_files_paths: list):
         input_files = []
         output_files = []
 
@@ -115,7 +114,7 @@ class MultiWayMerge:
         j = 0
         seq = []
         # main loop
-        while not self.is_all_empty(input_files):
+        while not self.are_all_empty(input_files):
             min_val = MAX_INT
             min_ind = -1
             # find the smallest number among all input files
